da58d31b1455173d4dab070a8d6dc6cb9babd577
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-12-18 21:58:23 -0800

    main page services picks updated for reactive text and position.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 import goldenRec from "../assets/goldenRec.jpg";
 import Docked from "../assets/Docked.jpeg";
 import Far from "../assets/Argrosy-far.jpeg";
-import Discovery from "../assets/Discovery-day.jpeg"
+import Discovery from "../assets/Discovery-day.jpeg";
 import { Container } from "react-bootstrap";
-
 
 // style={{backgroundRepeat: "no-repeat", backgroundImage: `url(${goldenRec})`, backgroundSize: ""}}
 
@@ -14,44 +13,65 @@ export default function Main() {
   return (
     <section>
       {/* about me container */}
-      <div className="" id="About">
-        <Container className=" capImg  d-flex flex-column"  >
+      <div id="About">
+        <Container className=" capImg  d-flex flex-column">
           <figure className="welcome">
-          <img alt="goldenRecImg" src={goldenRec}/>
-          <figcaption>WELCOME TO CAPTAIN HERB</figcaption> 
+            <img className="Gtriangle" alt="goldenRecImg" src={goldenRec} />
+            <figcaption>WELCOME TO CAPTAIN HERB</figcaption>
           </figure>
         </Container>
 
         {/* picture on about me picture and line break */}
         <div className="post-heading text-center justify-content-center">
-          <h3 className="display-4 font-weight-bold">U.S. COAST GUARD LICENSED</h3>
-        <hr className="w-50 mx-auto" />
+          <h3 className="display-4 font-weight-bold">
+            U.S. COAST GUARD LICENSED
+          </h3>
+          <hr className="w-50 mx-auto" />
         </div>
-        <Container className="mainServicePics" >
+
+        <Container className="mainServicePics ">
           <figure>
-          <img className="docked" src={Docked}  height="400px" width="300px" alt="about me pic" />
-          <figcaption>
-              <li>
-              somthing
-              </li>
-          </figcaption>
-          <img src={Far}  height="400px" width="300px" alt="about me pic" />
-          <img src={Discovery}  height="400px" width="300px" alt="about me pic" />
-        </figure>
+            <img src={Docked} height="400px" width="300px" alt="about me pic" />
+            <figcaption>
+              <div>INFORMATION</div>
+              <div>continued</div>
+            </figcaption>
+          </figure>
+
+          <figure>
+            <img src={Far} height="400px" width="300px" alt="about me pic" />
+            <figcaption>
+              <div>SERVICES</div>
+              <div>continued</div>
+            </figcaption>
+          </figure>
+          <figure>
+            <img
+              src={Discovery}
+              height="400px"
+              width="300px"
+              alt="about me pic"
+            />
+            <figcaption>
+              <div>CONTACT</div>
+              <div>continued</div>
+            </figcaption>
+          </figure>
         </Container>
 
         <div className=" text-center justify-content-center ">
           <div className="display-6 font-weight-bold">20 years experience </div>
-        <hr className="w-50 mx-auto" />
-          <div>Boating - Ownership - Boat Operations - Navigation - Systems - Maintainance</div>
+          <hr className="w-50 mx-auto" />
           <div>
-            <Button component={Link} to="/Services" id="Services">FIND OUT MORE</Button>
+            Boating - Ownership - Boat Operations - Navigation - Systems -
+            Maintainance
+          </div>
+          <div>
+            <Button component={Link} to="/Services" id="Services">
+              FIND OUT MORE
+            </Button>
           </div>
         </div>
-
-        
-
-       
       </div>
     </section>
   );
