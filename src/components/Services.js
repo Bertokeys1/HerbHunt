e5a25a0e471a4df893f22337df5e25ea19613ab0
Bertokeys1 <abarrientos93.ab@gmail.com>
2023-05-19 22:13:01 -0700

    added modal to service page for potential photo gallery

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,11 +4,30 @@ import Docks from "../assets/Docks.jpeg"
 import Malibu from "../assets/Malibu-docked.jpeg"
 import Docked from "../assets/Offshore.jpeg"
 import Wheel from "../assets/Wheel.jpeg"
-import { Button } from "@mui/material"
 import { Link } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 365,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 export default function Services() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div id="Services">
       <div  className="mt-5 pb-5">
@@ -31,7 +50,24 @@ export default function Services() {
           </div>
           <div className="serviceBlocks">
             <div className="serviceText">Racing/Competitive Sailing</div>
+
+            {/* modal for photo gallery */}
+          <Button onClick={handleOpen}>
           <img src={Docks}  height="270px" width="300px" alt="about me pic" />
+            </Button>
+            <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+          {/* photos */}
+        <Box sx={style}>
+        <img src={Docks}  height="270px" width="300px" alt="about me pic" />
+        </Box>
+      </Modal>
+
+
             <li>Drive and communicate with crew to keep vessel moving fast</li>
           </div>
         </div>
